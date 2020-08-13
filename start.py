ddf6dd0596feb6005542201d9abce41978660540
Emanuele Ballarin <emanuele@ballarin.cc>
2020-08-14 00:24:11 +0200

    ✨ Version bumps

--- start.py
+++ start.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python
+
+# Scientific stack
 import numpy
+import numpy as np
+import scipy
+import scipy as sp
+import pandas
+import pandas as pd
+import vaex
+import vaex as vx
+
+# Deep Learning stack
+import jax
+import torch
+import torch as th
+import pytorch_lightning
+import pytorch_lightning as pl
+import pl_bolts
+import pl_bolts as plb
