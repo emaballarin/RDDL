59910a0fe4c44b9a90a8c38526f830e42989b946
Emanuele Ballarin <emanuele@ballarin.cc>
2020-09-28 02:56:16 +0200

    ✨ "Omnibus" update

--- start.py
+++ start.py
@@ -2,7 +2,7 @@
 
 # Scientific stack
 import numpy
-import numpy as np
+#import numpy as np
 #import scipy
 #import scipy as sp
 #import pandas
@@ -12,9 +12,9 @@ import numpy as np
 
 # Deep Learning stack
 #import jax
-import torch
-import torch as th
-import pytorch_lightning
-import pytorch_lightning as pl
-import pl_bolts
-import pl_bolts as plb
+#import torch
+#import torch as th
+#import pytorch_lightning
+#import pytorch_lightning as pl
+#import pl_bolts
+#import pl_bolts as plb
