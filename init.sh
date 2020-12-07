5b4e0b8e0678ee2cb6c425c55ef9e0760872cc12
Emanuele Ballarin <emanuele@ballarin.cc>
2020-12-07 19:00:54 +0100

    ✨ Version bumps & bugfix PTGeometric

--- init.sh
+++ init.sh
@@ -137,10 +137,10 @@ cd ..
 
 export PRE_CUDA="$CUDA"
 export CUDA="cu110"
-pip install torch-scatter==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.7.0.html
-pip install torch-sparse==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.7.0.html
-pip install torch-cluster==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.7.0.html
-pip install torch-spline-conv==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.7.0.html
+pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.7.0+cu110.html
+pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-1.7.0+cu110.html
+pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-1.7.0+cu110.html
+pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.7.0+cu110.html
 pip install git+https://github.com/rusty1s/pytorch_geometric.git --no-deps
 pip install git+https://github.com/benedekrozemberczki/pytorch_geometric_temporal.git
 export CUDA="$PRE_CUDA"
